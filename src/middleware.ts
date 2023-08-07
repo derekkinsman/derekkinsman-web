a9386d68581ff670e7318d18bde1ffc81afff2c8
Derek J. Kinsman <derek@unttld.com>
2023-08-07 12:16:03 -0400

    Lint clean up.

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,12 +1,12 @@
-import { NextResponse } from 'next/server'
+import { NextResponse } from 'next/server';
 
 export function middleware() {
   // Store the response so we can modify its headers
-  const response = NextResponse.next()
+  const response = NextResponse.next();
 
   // Set custom header
-  response.headers.set('X-Clacks-Overhead', 'GNU Terry Pratchett')
+  response.headers.set('X-Clacks-Overhead', 'GNU Terry Pratchett');
 
   // Return response
-  return response
+  return response;
 }

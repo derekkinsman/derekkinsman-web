0abe4a87e051a3eec798808d197c20f69b0787b7
Derek J. Kinsman <derek@unttld.com>
2023-09-30 12:47:51 -0400

    Migrate to app router.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,5 @@
+import '@styles/_base.scss';
+
 export default function DashboardLayout({
   children,
 }: {
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,14 @@
+import Biography from '@components/pageBlocks/biography';
+import Contact from '@components/pageBlocks/contact';
+import Kindness from '@components/pageBlocks/kindness';
 import Masthead from '@components/pageBlocks/masthead';
 
 export default async function Page() {
 
   return <>
     <Masthead />
+    <Biography />
+    <Contact />
+    <Kindness />
   </>;
 }

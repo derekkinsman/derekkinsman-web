82489a119a7474f65165680ab7a69582b4aea272
Derek J. Kinsman <derek@unttld.com>
2023-10-01 16:07:07 -0400

    Fix dumb mistakes.

--- src/app/_components/pageBlocks/index.ts
+++ src/app/_components/pageBlocks/index.ts
@@ -1,6 +1,6 @@
-export { Biography } from './Biography';
-export { Copyright } from './Copyright';
-export { Elsewhere } from './Elsewhere';
-export { Kindness } from './Kindness';
-export { Masthead } from './Masthead';
-export { Place } from './Place';
+export type { Biography } from './Biography';
+export type { Copyright } from './Copyright';
+export type { Elsewhere } from './Elsewhere';
+export type { Kindness } from './Kindness';
+export type { Masthead } from './Masthead';
+export type { Place } from './Place';
